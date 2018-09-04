4950dbf10d1e21dc98fd14518648caf4bb831ae4
SARAT KUMAR BEHERA <beherasaratkumar@gmail.com>
2018-09-04 10:17:38 +0530

    Fixed smb2_list_shares:-
        As I changed the smb2_read and smb2_write to return status instead of
        bytes read/written, this needed a fix.
    
    	Added multi-credit support to IOCTL

--- lib/libsmb2.c
+++ lib/libsmb2.c
@@ -2089,7 +2089,15 @@ smb2_ioctl_async(struct smb2_context *smb2, struct smb2fh *fh,
         req.file_id.volatile_id= fh->file_id.volatile_id;
         req.flags = ioctl_flags;
         req.max_input_response = 0;
-        req.max_output_response = 64 * 1024;
+
+        /* For SMB 2.02 credit charge is 0 for IOCTL and accepted payload is around (63 KB),
+           though it works somehow with - (64*1024 - 108) bytes.
+
+           For higher SMB versions where multi credit is supported credit charge is to be calculated.
+           and  accepted payload is around max_transact_size - 1024 */
+
+        //req.max_output_response = smb2_get_max_transact_size(smb2) - 108;
+        req.max_output_response = smb2_get_max_transact_size(smb2) - 1024;
 
         pdu = smb2_cmd_ioctl_async(smb2, &req, ioctl_cb, ioctl_d);
         if (pdu == NULL) {
